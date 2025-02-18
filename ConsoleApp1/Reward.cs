9fd9083997a9d96eba2d3d967f08a140ccc074e4
MenikovMaximm <moderator2020@inbox.ru>
2025-02-18 20:50:46 +0300

    Add files via upload

---
+++ ConsoleApp1/Reward.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Reward : Character
+    {
+        public int Exp { get; set; }
+        public uint Health { get; set; }
+        public int Strong { get; set; }
+        public int Protected { get; set; }
+
+        public Reward(int exp, uint health, int strong, int protectedValue)
+        {
+            Exp = exp;
+            Health = health;
+            Strong = strong;
+            Protected = protectedValue;
+        }
+    }
+}
